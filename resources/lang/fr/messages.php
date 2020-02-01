8366f15fe73384598f95cb61fa9928a2edbc2277
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2020-02-01 05:08:04 -0500

    Add space in front of "!" (#3)

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -14,5 +14,5 @@ return [
     'no-site' => 'Aucun site de vote n\'est disponible pour le moment.',
     'vote-delay' => 'Vous avez déjà voté, vous pouvez voter à nouveau :time !',
     'site-no-rewards' => 'Ce site n\'a pas de récompenses.',
-    'vote-success' => 'Votre vote a été pris en compte, vous recevrez bientôt vos récompenses!',
+    'vote-success' => 'Votre vote a été pris en compte, vous recevrez bientôt vos récompenses !',
 ];
