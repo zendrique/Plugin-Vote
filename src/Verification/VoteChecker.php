2e8f9f50117b950d8d830c4be5c8b9370f976f8c
MrMicky <git@mrmicky.fr>
2020-02-01 00:37:38 +0000

    Apply fixes from StyleCI

--- src/Verification/VoteChecker.php
+++ src/Verification/VoteChecker.php
@@ -35,7 +35,7 @@ class VoteChecker
             'https://serveur-minecraft.fr/api-{id}_{ip}.json',
             'status',
             'Success');
-        
+
         $this->sites['liste-serveur.fr'] = $this->verifyByJson(
           'https://www.liste-serveur.fr/api/hasVoted/{server_token}/{ip}',
           'hasVoted',
